f49d7b085ab2a7af21a71103f25ad05adc1a72fa
yangfeng <yangfeng@123456>
2022-10-17 09:22:44 +0800

    自定义服务类

--- spring-first-demo/src/main/java/com/yf/config/AppConfig.java
+++ spring-first-demo/src/main/java/com/yf/config/AppConfig.java
@@ -2,7 +2,13 @@ package com.yf.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+/**
+ * @ClassName AppConfig
+ * @Description TODO
+ * @Author @yangfeng
+ * @Date 2022/10/16 8:15
+ * @Version 1.0
+ */
 @Configuration
 @ComponentScan("com.yf")
 public class AppConfig {
---
+++ spring-first-demo/src/main/java/com/yf/test/A.java
@@ -0,0 +1,11 @@
+package com.yf.test;
+
+/**
+ * @ClassName A
+ * @Description TODO
+ * @Author @yangfeng
+ * @Date 2022/10/16 8:15
+ * @Version 1.0
+ */
+public class A {
+}
